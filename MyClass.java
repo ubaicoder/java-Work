fe8775b1118233043be83afa3daf91405f9a340b
Ubai Ratangiri <112539947+ubaicoder@users.noreply.github.com>
2024-10-18 15:26:12 +0530

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -2,18 +2,8 @@ import java.util.Scanner;
 
 public class MyClass {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+       System.out.println("This is Ubai Ratangiri");
         
-        System.out.println("Type a number:");
-        
-        // Check if input is available
-        if (scanner.hasNextInt()) {
-            int number = scanner.nextInt();
-            System.out.println("You typed: " + number);
-        } else {
-            System.out.println("No input provided. Exiting...");
-        }
-
-        scanner.close();
+      
     }
 }
